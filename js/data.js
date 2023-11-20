2bcf4538f9dfb6bb8aed9dcea27a4da40a8d5e24
haralampo <91089966+haralampo@users.noreply.github.com>
2023-11-19 20:39:19 -0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,22 @@
+let songs = [
+    {
+        name: "Picture To Burn",
+        path: 'music/song1.mp3',
+        artist: 'Taylor Swift',
+        cover: 'musicimages/cover1.png'
+    },
+
+    {
+        name: "Cold As You",
+        path: 'music/song2.mp3',
+        artist: 'Taylor Swift',
+        cover: 'musicimages/cover1.png'
+    },
+
+    {
+        name: "Should've Said No",
+        path: 'music/song3.mp3',
+        artist: 'Taylor Swift',
+        cover: 'musicimages/cover1.png'
+    },
+]
